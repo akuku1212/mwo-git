2afd6d413bc13451d3cfe64575402a11720fafd6
daniel s <szymulanski@student.agh.edu.pl>
2017-02-11 14:12:41 +0100

    rockets implementation

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,4 +3,8 @@ public class SomeProgram {
         System.out.println("We are learning to use Git.");
 	System.out.println("We are insane!");
     }
+
+	public void manageRockets() {
+    // TODO implement
+	}
 }
